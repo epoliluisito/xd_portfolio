e6f4f6354e9ec51e581e6bb343b85ad42408103a
epoliluisito <epoli.uk@gmail.com>
2026-07-30 13:09:58 +0200

    delete
    delete

--- projects/Kakaja/serve.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Kakaja dev server. ES modules can't load from file://, so serve the folder.
-cd "$(dirname "$0")" || exit 1
-echo "Kakaja → http://localhost:8080  (ctrl-C to stop)"
-python3 -m http.server 8080
